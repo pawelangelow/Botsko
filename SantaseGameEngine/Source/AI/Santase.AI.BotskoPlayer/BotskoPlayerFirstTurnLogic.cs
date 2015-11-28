c5e5a5962a7f81c283d692638e894784a548cd5b
InKolev <in.kolev.b@gmail.com>
2015-11-28 16:49:06 +0200

    Override Execute method for the two case logics.

--- SantaseGameEngine/Source/AI/Santase.AI.BotskoPlayer/BotskoPlayerFirstTurnLogic.cs
+++ SantaseGameEngine/Source/AI/Santase.AI.BotskoPlayer/BotskoPlayerFirstTurnLogic.cs
@@ -5,8 +5,15 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using Logic.Cards;
+    using Logic.PlayerActionValidate;
+    using Logic.Players;
 
     public class BotskoPlayerFirstTurnLogic : BotskoPlayerCommonLogic
     {
+        public override Card Execute(PlayerTurnContext context, IPlayerActionValidator playerActionValidator, ICollection<Card> cards)
+        {
+            return base.Execute(context, playerActionValidator, cards);
+        }
     }
 }
