6ca45cb636bcacb68447941a4ef6ae71b42cea83
InKolev <in.kolev.b@gmail.com>
2015-11-28 16:16:49 +0200

    Initial Structure.

---
+++ SantaseGameEngine/Source/AI/Santase.AI.BotskoPlayer/BotskoPlayer.cs
@@ -0,0 +1,52 @@
+﻿namespace Santase.AI.BotskoPlayer
+{
+    using System.Linq;
+
+    using Santase.Logic.Extensions;
+    using Santase.Logic.Players;
+
+    /// <summary>
+    /// This dummy player follows the rules and always plays random card.
+    /// Dummy never changes the trump or closes the game.
+    /// </summary>
+    // ReSharper disable once UnusedMember.Global
+    public class BotskoPlayer : BasePlayer
+    {
+        public BotskoPlayer()
+            : this("Botsko Player")
+        {
+        }
+
+        public BotskoPlayer(string name)
+        {
+            this.Name = name;
+        }
+
+        public override string Name { get; }
+
+        public override PlayerAction GetTurn(PlayerTurnContext context)
+        {
+            if (context.IsFirstPlayerTurn)
+            {
+                // Az igraq purvi
+                // Tuk ne pipa Ivan, shtoto bolqt zubi.
+            }
+            else
+            {
+                // Az igraq vtori 
+                // Tuk pipa Ivan, shtoto si misli che znae kvo pravi.
+            }
+
+            // Gets the cards in our hand
+            var possibleCardsToPlay = this.PlayerActionValidator.GetPossibleCardsToPlay(context, this.Cards);
+            var shuffledCards = possibleCardsToPlay.Shuffle();
+            var cardToPlay = shuffledCards.First();
+
+            // SecondPlayer == Opponent
+            var opponentCardType = context.SecondPlayedCard.Type;
+            var opponentCardSuit = context.SecondPlayedCard.Suit;
+
+            return this.PlayCard(cardToPlay);
+        }
+    }
+}
---
+++ SantaseGameEngine/Source/AI/Santase.AI.DummyPlayer/DummyPlayer.cs
@@ -0,0 +1,52 @@
+﻿namespace Santase.AI.DummyPlayer
+{
+    using System.Linq;
+
+    using Santase.Logic.Extensions;
+    using Santase.Logic.Players;
+
+    /// <summary>
+    /// This dummy player follows the rules and always plays random card.
+    /// Dummy never changes the trump or closes the game.
+    /// </summary>
+    // ReSharper disable once UnusedMember.Global
+    public class DummyPlayer : BasePlayer
+    {
+        public DummyPlayer()
+            : this("Dummy Player Lvl. 1")
+        {
+        }
+
+        public DummyPlayer(string name)
+        {
+            this.Name = name;
+        }
+
+        public override string Name { get; }
+
+        public override PlayerAction GetTurn(PlayerTurnContext context)
+        {
+            if (context.IsFirstPlayerTurn)
+            {
+                // Az igraq purvi
+                // Tuk ne pipa Ivan, shtoto bolqt zubi.
+            }
+            else
+            {
+                // Az igraq vtori 
+                // Tuk pipa Ivan, shtoto si misli che znae kvo pravi.
+            }
+
+            // Gets the cards in our hand
+            var possibleCardsToPlay = this.PlayerActionValidator.GetPossibleCardsToPlay(context, this.Cards);
+            var shuffledCards = possibleCardsToPlay.Shuffle();
+            var cardToPlay = shuffledCards.First();
+
+            // SecondPlayer == Opponent
+            var opponentCardType = context.SecondPlayedCard.Type;
+            var opponentCardSuit = context.SecondPlayedCard.Suit;
+
+            return this.PlayCard(cardToPlay);
+        }
+    }
+}
