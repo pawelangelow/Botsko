b5848bacf51af15d1e0462c835ec03e1c471e53c
Pavel <pawel.angelow@gmail.com>
2015-11-29 16:54:05 +0200

    More changes in decision of closing game

---
+++ SantaseGameEngine/Source/AI/Santase.AI.BotskoPlayer/HandSummary.cs
@@ -0,0 +1,13 @@
+﻿namespace Santase.AI.BotskoPlayer
+{
+    internal class HandSummary
+    {
+        public int CountOfTrumps { get; set; }
+
+        public int PointsOfAll { get; set; }
+
+        public int PointsOfTrumps { get; set; }
+
+        public int CountOfAcesNoTrumps { get; set; }
+    }
+}
